7844c0339df7cfa359cfea347f277b57f30b7412
YJ <zerosix1017@gmail.com>
2024-03-12 21:42:11 +0900

    Design Pattern #7 1. bridge pattern

---
+++ src/bridge/Main.java
@@ -0,0 +1,14 @@
+package bridge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Display d1 = new Display(new StringDisplayImpl("Hello"));
+        Display d2 = new CountDisplay(new StringDisplayImpl("Hello, world"));
+        CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, people"));
+        d1.display();
+        d2.display();
+        d3.display();
+        d3.multiDisplay(5);
+    }
+}
